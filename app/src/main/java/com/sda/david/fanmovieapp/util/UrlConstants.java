2be09bb928a2dea3c4cd465ab4170b8c8dd97285
David <david.dmr@gmail.com>
2017-04-29 16:55:34 -0300

    Adição de endpoints ao arquivo de constantes

---
+++ app/src/main/java/com/sda/david/fanmovieapp/util/UrlConstants.java
@@ -0,0 +1,25 @@
+package com.sda.david.fanmovieapp.util;
+
+/**
+ * Created by david on 29/04/2017.
+ */
+
+public class UrlConstants {
+
+    //Movie
+    public static final String URL_MOVIE_FIND_ONE = "/movie";
+    public static final String URL_MOVIE_FIND_ALL = "/movie/list";
+    public static final String URL_MOVIE_ADD = "/movie/add";
+    public static final String URL_MOVIE_REMOVE = "/movie/remove";
+    public static final String URL_MOVIE_TOP_10 = "/movie/top10";
+
+    //User
+    public static final String URL_USER_ONE = "/user/";
+    public static final String URL_USER_FIND_ALL = "/user/list";
+    public static final String URL_USER_ADD = "/user/add";
+    public static final String URL_USER_REMOVE = "/user/remove";
+    public static final String URL_USER_ADD_FAVORITE = "/user/add_favorite";
+    public static final String URL_USER_REMOVE_FAVORITE = "/user/remove_favorite";
+    public static final String URL_USER_FAVORITES = "/user/favorites";
+
+}
