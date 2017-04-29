62fe924ca81cfffeb4b0ede974d2c82c2aef9eda
David <david.dmr@gmail.com>
2017-04-29 13:33:41 -0300

    Adição de delay ao carregar a aplicação

--- app/src/main/java/com/sda/david/fanmovieapp/SplashActivity.java
+++ app/src/main/java/com/sda/david/fanmovieapp/SplashActivity.java
@@ -2,7 +2,10 @@ package com.sda.david.fanmovieapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
+import com.sda.david.fanmovieapp.login.LoginActivity;
 
 /**
  * Created by david on 29/04/2017.
@@ -14,6 +17,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                callLoginScreen();
+            }
+        }, 2000);
+
+    }
+
+    private void callLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void callHomeScreen() {
         Intent intent = new Intent(this, BaseActivity.class);
         startActivity(intent);
         finish();
