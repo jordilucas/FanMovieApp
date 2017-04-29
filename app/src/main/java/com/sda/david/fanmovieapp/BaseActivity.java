3e90c1186318e3ba44a7a61c2f363575795f42ba
David <david.dmr@gmail.com>
2017-04-29 16:43:52 -0300

    função de logout

--- app/src/main/java/com/sda/david/fanmovieapp/BaseActivity.java
+++ app/src/main/java/com/sda/david/fanmovieapp/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.sda.david.fanmovieapp;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.sda.david.fanmovieapp.login.LoginActivity;
 import com.sda.david.fanmovieapp.movies.MovieFragment;
 
 public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -95,7 +97,9 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
         } else if (id == R.id.nav_register) {
 
         } else if (id == R.id.nav_logout) {
-
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         }
 
 
