fad839cb7559db863a752b5433c0f119cd707fc0
David <david.dmr@gmail.com>
2017-04-29 19:10:52 -0300

    Implementação da listagem dos filmes na tela de todos os filmes

---
+++ app/src/main/java/com/sda/david/fanmovieapp/movies/MovieAdapter.java
@@ -0,0 +1,84 @@
+package com.sda.david.fanmovieapp.movies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.sda.david.fanmovieapp.R;
+import com.sda.david.fanmovieapp.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by david on 29/04/2017.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private String TAG = "MovieAdp";
+    private List<Movie> movieList;
+    private Context ctx;
+    private LayoutInflater mLayoutInflater;
+    private View.OnClickListener mClickListener;
+
+    public MovieAdapter(Context ctx, List<Movie> movieList, View.OnClickListener mClickListener) {
+        this.ctx = ctx;
+        this.movieList = movieList;
+        this.mClickListener = mClickListener;
+        mLayoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mLayoutInflater.inflate(R.layout.adapter_movie_item, parent, false);
+
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+
+        holder.mainLayout.setOnClickListener(mClickListener);
+        holder.mainLayout.setId(position);
+
+        String urlMoviePoster = movieList.get(position).getPosterPath();
+        if(urlMoviePoster != null && urlMoviePoster.charAt(0) == '/')
+            urlMoviePoster = "http://image.tmdb.org/t/p/w92" + urlMoviePoster;
+
+        Picasso
+            .with(ctx)
+            .load(urlMoviePoster)
+            .into(holder.ivMoviePoster);
+
+        holder.tvMovieTitle.setText(movieList.get(position).getTitle());
+        holder.tvMovieNote.setText(String.valueOf(movieList.get(position).getVoteAverage()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        RelativeLayout mainLayout;
+        ImageView ivMoviePoster;
+        TextView tvMovieTitle;
+        TextView tvMovieNote;
+
+        MovieViewHolder(View itemView) {
+            super(itemView);
+
+            mainLayout = (RelativeLayout) itemView.findViewById(R.id.main_layout);
+            ivMoviePoster = (ImageView) itemView.findViewById(R.id.iv_movie_poster);
+            tvMovieTitle = (TextView) itemView.findViewById(R.id.tv_movie_title);
+            tvMovieNote = (TextView) itemView.findViewById(R.id.tv_movie_note);
+        }
+    }
+}
