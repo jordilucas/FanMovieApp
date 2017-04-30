96d10b3e257ba62b2bd3f95c810c0c081b9feaa0
David <david.dmr@gmail.com>
2017-04-30 17:51:41 -0300

    Abrindo por padrão a tela home

--- app/src/main/java/com/sda/david/fanmovieapp/SplashActivity.java
+++ app/src/main/java/com/sda/david/fanmovieapp/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                callLoginScreen();
+                callHomeScreen();
             }
         }, 2000);
 
