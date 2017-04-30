ceb60b165a157c7853a08ba07cd686a883ff22aa
David <david.dmr@gmail.com>
2017-04-30 13:46:47 -0300

    Implementação de lista de filmes na tela de Favoritos

--- app/src/main/java/com/sda/david/fanmovieapp/favorites/FavoritesFragment.java
+++ app/src/main/java/com/sda/david/fanmovieapp/favorites/FavoritesFragment.java
@@ -3,11 +3,26 @@ package com.sda.david.fanmovieapp.favorites;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.sda.david.fanmovieapp.R;
+import com.sda.david.fanmovieapp.api.ServiceGenerator;
+import com.sda.david.fanmovieapp.api.UserService;
+import com.sda.david.fanmovieapp.model.Movie;
+import com.sda.david.fanmovieapp.movies.MovieAdapter;
+
+import java.util.Collections;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by david on 29/04/2017.
@@ -19,6 +34,10 @@ public class FavoritesFragment extends Fragment {
     private static final String ARG_1 = "arg_1";
 
     private boolean arg1;
+
+    RecyclerView rvMovies;
+
+    List<Movie> movies;
 
     public static FavoritesFragment newInstance() {
         FavoritesFragment fragment = new FavoritesFragment();
@@ -43,13 +62,56 @@ public class FavoritesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
-        initComponents();
+        initComponents(rootView);
 
         return rootView;
     }
 
-    private void initComponents() {
+    private void initComponents(View rootView) {
+        rvMovies = (RecyclerView) rootView.findViewById(R.id.rv_movies);
+        rvMovies.setHasFixedSize(true);
+        LinearLayoutManager mLinearLayoutManager = new GridLayoutManager(getContext(), 3);
+        rvMovies.setLayoutManager(mLinearLayoutManager);
 
+        userListFavorites();
+
+    }
+
+    private void fillScreen() {
+        MovieAdapter adapter = new MovieAdapter(getContext(), movies, movieClickListener());
+        rvMovies.setAdapter(adapter);
+    }
+
+    private View.OnClickListener movieClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = view.getId();
+                Log.d(TAG, "onClick: " + position);
+            }
+        };
+    }
+
+    private void userListFavorites() {
+        Call<List<Movie>> call = ServiceGenerator.createService(UserService.class).findFavorites((long) 44);
+        call.enqueue(new Callback<List<Movie>>() {
+            @Override
+            public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
+                if(response.isSuccessful()) {
+                    movies = response.body();
+                    movies.removeAll(Collections.<Movie>singleton(null));
+                    fillScreen();
+                } else {
+                    Log.d(TAG, "onError: ");
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Movie>> call, Throwable t) {
+                Log.d(TAG, "onFailure: ");
+            }
+        });
     }
 
 }
