c660f3f2294feb7fa03fc5decd8f6058d03e4fa4
David <david.dmr@gmail.com>
2017-05-01 20:29:16 -0300

    Implementação de parcelable na classe User

--- app/src/main/java/com/sda/david/fanmovieapp/model/User.java
+++ app/src/main/java/com/sda/david/fanmovieapp/model/User.java
@@ -1,14 +1,19 @@
 package com.sda.david.fanmovieapp.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Created by david on 29/04/2017.
  */
 
-public class User {
+public class User implements Parcelable {
 
     private Long id;
 
@@ -20,7 +25,7 @@ public class User {
     private String password;
 
     @SerializedName("favorites_movies")
-    private Set<Long> favoritesMovies;
+    private List<Long> favoritesMovies;
 
     private boolean administrator;
 
@@ -32,7 +37,7 @@ public class User {
         this.password = password;
     }
 
-    public User(String name, String userName, String password, Set<Long> favoritesMovies, boolean administrator) {
+    public User(String name, String userName, String password, List<Long> favoritesMovies, boolean administrator) {
         this.name = name;
         this.userName = userName;
         this.password = password;
@@ -40,7 +45,7 @@ public class User {
         this.administrator = administrator;
     }
 
-    public User(Long id, String name, String userName, Set<Long> favoritesMovies, boolean administrator) {
+    public User(Long id, String name, String userName, List<Long> favoritesMovies, boolean administrator) {
         this.id = id;
         this.name = name;
         this.userName = userName;
@@ -64,11 +69,49 @@ public class User {
         return password;
     }
 
-    public Set<Long> getFavoritesMovies() {
-        return favoritesMovies;
-    }
+//    public List<Long> getFavoritesMovies() {
+//        return favoritesMovies;
+//    }
 
     public boolean isAdministrator() {
         return administrator;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.id);
+        dest.writeString(this.name);
+        dest.writeString(this.userName);
+        dest.writeString(this.password);
+        dest.writeList(this.favoritesMovies);
+        dest.writeByte(this.administrator ? (byte) 1 : (byte) 0);
+    }
+
+    protected User(Parcel in) {
+        this.id = (Long) in.readValue(Long.class.getClassLoader());
+        this.name = in.readString();
+        this.userName = in.readString();
+        this.password = in.readString();
+        this.favoritesMovies = new ArrayList<Long>();
+        in.readList(this.favoritesMovies, Long.class.getClassLoader());
+        this.administrator = in.readByte() != 0;
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
