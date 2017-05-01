209c21e923dbf0871dcfbc4abf9d7e7331c112e1
David <david.dmr@gmail.com>
2017-05-01 15:00:03 -0300

    Criação de util para exibir mensagens nas telas

---
+++ app/src/main/java/com/sda/david/fanmovieapp/util/ShowMessageUtil.java
@@ -0,0 +1,103 @@
+package com.sda.david.fanmovieapp.util;
+
+import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by david on 01/05/2017.
+ */
+public class ShowMessageUtil {
+
+    public static void showMessage(Context ctx, String message) {
+        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void longSnackBar(View view, String message) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    public static void longSnackBarWithDuration(View view, String message, int duration) {
+        Snackbar snackbar = Snackbar.make(view, message, duration);
+        View snackbarView = snackbar.getView();
+        TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setMaxLines(5);
+        snackbar.show();
+    }
+
+    public static void longSnackBar(View view, String message, int snackBarColor) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(snackBarColor);
+        snackbar.show();
+    }
+
+    public static void longSnackBar(View view, int message) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    public static void longSnackBar(View view, int message, int snackBarColor) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(snackBarColor);
+        snackbar.show();
+    }
+
+    public static void snackBarAction(View view, String message, String actionName, View.OnClickListener action) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionName, action)
+                .show();
+    }
+
+    public static void snackBarAction(View view, String message, String actionName, View.OnClickListener action, int snackBarColor) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionName, action);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(snackBarColor);
+        snackbar.show();
+    }
+
+    public static void snackBarAction(View view, String message, int actionName, View.OnClickListener action) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionName, action)
+                .show();
+    }
+
+    public static void snackBarAction(View view, String message, int actionName, View.OnClickListener action, int snackBarColor) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionName, action);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(snackBarColor);
+        snackbar.show();
+    }
+
+    public static void snackBarAction(View view, int message, int actionName, View.OnClickListener action) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionName, action)
+                .show();
+    }
+
+    public static void snackBarAction(View view, int message, int actionName, View.OnClickListener action, int snackBarColor) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionName, action);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(snackBarColor);
+        snackbar.show();
+    }
+
+    public static void snackBarAction(View view, int message, String actionName, View.OnClickListener action) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionName, action)
+                .show();
+    }
+
+    public static void snackBarAction(View view, int message, String actionName, View.OnClickListener action, int snackBarColor) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionName, action);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(snackBarColor);
+        snackbar.show();
+    }
+}
