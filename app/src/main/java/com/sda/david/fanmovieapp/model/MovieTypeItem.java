2478ffc702af7c751d3545f2332f9c9109a7571d
David Melo <david.dmr@gmail.com>
2017-05-04 19:22:39 -0300

    Criação de tipo para unir informações que serão exibidas no type title e content na tela de gêneros

--- app/src/main/java/com/sda/david/fanmovieapp/model/MovieTypeItem.java
+++ app/src/main/java/com/sda/david/fanmovieapp/model/MovieTypeItem.java
@@ -1,8 +1,43 @@
 package com.sda.david.fanmovieapp.model;
 
+import java.util.List;
+
 /**
  * Created by david.melo on 04/05/2017.
  */
 
 public class MovieTypeItem {
+
+    private List<Movie> movies;
+
+    private String genreTitle;
+
+    private Type type;
+
+    public MovieTypeItem(String genreTitle, Type type) {
+        this.genreTitle = genreTitle;
+        this.type = type;
+    }
+
+    public MovieTypeItem(List<Movie> movies, Type type) {
+        this.movies = movies;
+        this.type = type;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public String getGenreTitle() {
+        return genreTitle;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public enum Type{
+        TITLE, CONTENT;
+    }
+
 }
