4505fc693aeb1bc550d5eb7059682520c9c16d6b
David <david.dmr@gmail.com>
2017-05-04 00:35:27 -0300

    Adição de searchview e suas funcionalidades na tela de filmes

--- app/src/main/java/com/sda/david/fanmovieapp/movies/MovieFragment.java
+++ app/src/main/java/com/sda/david/fanmovieapp/movies/MovieFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,7 @@ import com.sda.david.fanmovieapp.api.interfaces.MovieService;
 import com.sda.david.fanmovieapp.api.ServiceGenerator;
 import com.sda.david.fanmovieapp.model.Movie;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -34,6 +36,7 @@ public class MovieFragment extends Fragment {
 
     private boolean arg1;
 
+    SearchView svMovie;
     RecyclerView rvMovies;
 
     List<Movie> movies;
@@ -67,6 +70,8 @@ public class MovieFragment extends Fragment {
     }
 
     private void initComponents(View rootView) {
+        svMovie = (SearchView) rootView.findViewById(R.id.sv_movie);
+        svMovie.setOnQueryTextListener(onQueryTextListener());
         rvMovies = (RecyclerView) rootView.findViewById(R.id.rv_movies);
         rvMovies.setHasFixedSize(true);
         LinearLayoutManager mLinearLayoutManager = new GridLayoutManager(getContext(), 3);
@@ -76,8 +81,41 @@ public class MovieFragment extends Fragment {
 
     }
 
-    private void fillScreen() {
+    private SearchView.OnQueryTextListener onQueryTextListener() {
+        return new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                updateAdapter(returnSearchMovieList(query));
 
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                updateAdapter(returnSearchMovieList(newText));
+
+                return false;
+            }
+        };
+    }
+
+    private List<Movie> returnSearchMovieList(String queryText) {
+        List<Movie> listToSearch = new ArrayList<>();
+        for(Movie movie : movies) {
+            if(movie.getTitle().toLowerCase().contains(queryText.toLowerCase())
+                    || movie.getOriginalTitle().toLowerCase().contains(queryText.toLowerCase())
+                    && !listToSearch.contains(movie)) {
+                listToSearch.add(movie);
+            }
+        }
+
+        return listToSearch;
+    }
+
+    private void updateAdapter(List<Movie> movies) {
+        MovieAdapter adapter = new MovieAdapter(getContext(), movies, movieClickListener());
+        rvMovies.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 
     private View.OnClickListener movieClickListener() {
@@ -101,9 +139,7 @@ public class MovieFragment extends Fragment {
             public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
                 if(response.isSuccessful()) {
                     movies = response.body();
-                    MovieAdapter adapter = new MovieAdapter(getContext(), movies, movieClickListener());
-                    rvMovies.setAdapter(adapter);
-                    fillScreen();
+                    updateAdapter(movies);
                 } else {
                     Log.d(TAG, "onError: ");
                 }
