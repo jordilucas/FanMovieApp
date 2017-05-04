0a3cd4caee6f4b1495a7e5d89f931e3231c8e434
David <david.dmr@gmail.com>
2017-05-03 22:30:01 -0300

    Refatoração do ServiceGenerator para suportar requisições de login com basic authentication

--- app/src/main/java/com/sda/david/fanmovieapp/api/ServiceGenerator.java
+++ app/src/main/java/com/sda/david/fanmovieapp/api/ServiceGenerator.java
@@ -1,5 +1,7 @@
 package com.sda.david.fanmovieapp.api;
 
+import android.text.TextUtils;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
@@ -10,6 +12,7 @@ import com.google.gson.JsonParseException;
 import java.lang.reflect.Type;
 import java.util.Date;
 
+import okhttp3.Credentials;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -22,7 +25,7 @@ public class ServiceGenerator {
 
     private static final String API_BASE_URL_DEVELOPMENT = "http://192.168.0.102:8080";
 
-    private static OkHttpClient httpClient = new OkHttpClient();
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
     private static Gson gson = new GsonBuilder().registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
         public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
@@ -35,8 +38,36 @@ public class ServiceGenerator {
                     .baseUrl(API_BASE_URL_DEVELOPMENT)
                     .addConverterFactory(GsonConverterFactory.create());
 
+    private static Retrofit retrofit = builder.build();
+
     public static <S> S createService(Class<S> serviceClass) {
-        Retrofit retrofit = builder.client(httpClient).build();
+        return retrofit.create(serviceClass);
+    }
+
+    public static <S> S createService(
+            Class<S> serviceClass, String username, String password) {
+        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)) {
+            String authToken = Credentials.basic(username, password);
+            return createService(serviceClass, authToken);
+        }
+
+        return createService(serviceClass, null, null);
+    }
+
+    public static <S> S createService(
+            Class<S> serviceClass, final String authToken) {
+        if (!TextUtils.isEmpty(authToken)) {
+            AuthenticationInterceptor interceptor =
+                    new AuthenticationInterceptor(authToken);
+
+            if (!httpClient.interceptors().contains(interceptor)) {
+                httpClient.addInterceptor(interceptor);
+
+                builder.client(httpClient.build());
+                retrofit = builder.build();
+            }
+        }
+
         return retrofit.create(serviceClass);
     }
 
