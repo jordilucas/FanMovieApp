0150d804725f2c35a1b0d393843342e1c251e8a5
David Melo <david.dmr@gmail.com>
2017-05-04 19:34:17 -0300

    Implementação de adapter para exibir as informações de título e lista de filmes por gênero na tela de gêneros

--- app/src/main/java/com/sda/david/fanmovieapp/movie_genre/MovieGenreAdapter.java
+++ app/src/main/java/com/sda/david/fanmovieapp/movie_genre/MovieGenreAdapter.java
@@ -1,6 +1,7 @@
 package com.sda.david.fanmovieapp.movie_genre;
 
 import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,83 +10,107 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.azoft.carousellayoutmanager.CarouselLayoutManager;
+import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
 import com.sda.david.fanmovieapp.R;
 import com.sda.david.fanmovieapp.model.Movie;
+import com.sda.david.fanmovieapp.model.MovieTypeItem;
+import com.sda.david.fanmovieapp.util.MovieGenre;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by david on 29/04/2017.
+ * Created by david on 04/05/2017.
  */
 
 public class MovieGenreAdapter extends RecyclerView.Adapter<MovieGenreAdapter.MovieViewHolder> {
 
-    private String TAG = "MovieAdp";
-    private List<Movie> movieList;
+    private String TAG = "MovieGenreAdp";
+    private List<MovieTypeItem> movieTypeItems;
     private Context ctx;
     private LayoutInflater mLayoutInflater;
     private View.OnClickListener mClickListener;
 
-    public MovieGenreAdapter(Context ctx, List<Movie> movieList, View.OnClickListener mClickListener) {
+    public MovieGenreAdapter(Context ctx, List<MovieTypeItem> movieTypeItems, View.OnClickListener mClickListener) {
         this.ctx = ctx;
-        this.movieList = movieList;
+        this.movieTypeItems = movieTypeItems;
         this.mClickListener = mClickListener;
         mLayoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mLayoutInflater.inflate(R.layout.adapter_movie_item, parent, false);
-
-        return new MovieViewHolder(view);
+        View view;
+        if(viewType == 0) {
+            view = mLayoutInflater.inflate(R.layout.adapter_movie_genre_title_item, parent, false);
+            return new TitleViewHolder(view);
+        } else {
+            view = mLayoutInflater.inflate(R.layout.adapter_movie_genre_content_item, parent, false);
+            return new ContentViewHolder(view);
+        }
     }
 
     @Override
     public void onBindViewHolder(MovieViewHolder holder, int position) {
+        if(holder.getItemViewType() == 0) {
+            TitleViewHolder titleViewHolder = (TitleViewHolder) holder;
+            titleViewHolder.tvGenreTitle.setText(movieTypeItems.get(position).getGenreTitle());
+        } else {
+            ContentViewHolder contentViewHolder = (ContentViewHolder) holder;
+            contentViewHolder.rvMoviesGenreInsider.setHasFixedSize(true);
+//            final CarouselLayoutManager mLinearLayoutManager = new CarouselLayoutManager(CarouselLayoutManager.HORIZONTAL, true);
+//            mLinearLayoutManager.setPostLayoutListener(new CarouselZoomPostLayoutListener());
+            LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(ctx);
+            mLinearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+            contentViewHolder.rvMoviesGenreInsider.setLayoutManager(mLinearLayoutManager);
+            MovieGenreInternalAdapter adapter = new MovieGenreInternalAdapter(ctx, movieTypeItems.get(position).getMovies(), mClickListener);
+            contentViewHolder.rvMoviesGenreInsider.setAdapter(adapter);
+        }
 
-        holder.mainLayout.setOnClickListener(mClickListener);
-        holder.mainLayout.setId(position);
-
-        String urlMoviePoster = "!";
-
-        if(movieList.get(position) != null && movieList.get(position).getPosterPath() != null)
-            urlMoviePoster = movieList.get(position).getPosterPath();
-
-        if(!urlMoviePoster.isEmpty() && urlMoviePoster.charAt(0) == '/')
-            urlMoviePoster = "http://image.tmdb.org/t/p/w92" + urlMoviePoster;
-
-        Picasso
-            .with(ctx)
-            .load(urlMoviePoster)
-            .into(holder.ivMoviePoster);
-
-        if(movieList.get(position) != null && movieList.get(position).getTitle() != null)
-            holder.tvMovieTitle.setText(movieList.get(position).getTitle());
-
-        if(movieList.get(position) != null && movieList.get(position).getVoteAverage() != 0f)
-            holder.tvMovieNote.setText(String.valueOf(movieList.get(position).getVoteAverage()));
     }
 
     @Override
     public int getItemCount() {
-        return movieList.size();
+        return movieTypeItems.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(movieTypeItems.get(position).getType().equals(MovieTypeItem.Type.TITLE)) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 
     class MovieViewHolder extends RecyclerView.ViewHolder {
 
-        RelativeLayout mainLayout;
-        ImageView ivMoviePoster;
-        TextView tvMovieTitle;
-        TextView tvMovieNote;
-
         MovieViewHolder(View itemView) {
             super(itemView);
+        }
+    }
 
-            mainLayout = (RelativeLayout) itemView.findViewById(R.id.main_layout);
-            ivMoviePoster = (ImageView) itemView.findViewById(R.id.iv_movie_poster);
-            tvMovieTitle = (TextView) itemView.findViewById(R.id.tv_movie_title);
-            tvMovieNote = (TextView) itemView.findViewById(R.id.tv_movie_note);
+    private class TitleViewHolder extends MovieViewHolder {
+
+        TextView tvGenreTitle;
+
+        TitleViewHolder(View itemView) {
+            super(itemView);
+
+            tvGenreTitle = (TextView) itemView.findViewById(R.id.tv_genre_title);
+        }
+    }
+
+    private class ContentViewHolder extends MovieViewHolder {
+
+        RecyclerView rvMoviesGenreInsider;
+
+        ContentViewHolder(View itemView) {
+            super(itemView);
+
+            rvMoviesGenreInsider = (RecyclerView) itemView.findViewById(R.id.rv_movies_genre_insider);
         }
     }
 }
