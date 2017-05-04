3c4b50468898897d2053cb69c8ad80a70682bcfa
David Melo <david.dmr@gmail.com>
2017-05-04 14:26:15 -0300

    Customização da tab bar na tela de movie detail

--- app/src/main/java/com/sda/david/fanmovieapp/movies/MovieDetailActivity.java
+++ app/src/main/java/com/sda/david/fanmovieapp/movies/MovieDetailActivity.java
@@ -6,9 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.sda.david.fanmovieapp.R;
 import com.sda.david.fanmovieapp.model.Movie;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by david on 02/05/2017.
@@ -19,6 +22,8 @@ public class MovieDetailActivity extends AppCompatActivity {
     public static final String ARG_MOVIE = "arg_movie";
 
     CollapsingToolbarLayout mCollapsingToolbarLayout;
+    ImageView ivMoviePoster;
+    TextView tvMovieOverview;
 
     private Movie movie;
 
@@ -55,7 +60,8 @@ public class MovieDetailActivity extends AppCompatActivity {
     }
 
     private void initComponents() {
-
+        ivMoviePoster = (ImageView) findViewById(R.id.iv_movie_backdrop);
+        tvMovieOverview = (TextView) findViewById(R.id.tv_movie_overview);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +74,24 @@ public class MovieDetailActivity extends AppCompatActivity {
     private void fillFieldsOnScreen() {
         if(movie != null) {
             mCollapsingToolbarLayout.setTitle(movie.getTitle());
+
+            String urlMoviePoster = "!";
+
+            if(movie.getBackdropPath() != null)
+                urlMoviePoster = movie.getBackdropPath();
+
+            if(!urlMoviePoster.isEmpty() && urlMoviePoster.charAt(0) == '/')
+                urlMoviePoster = "http://image.tmdb.org/t/p/w500" + urlMoviePoster;
+
+            Picasso
+                    .with(this)
+                    .load(urlMoviePoster)
+                    .into(ivMoviePoster);
+
+            tvMovieOverview.setText(movie.getOverview());
+
+        } else {
+            finish();
         }
     }
 
