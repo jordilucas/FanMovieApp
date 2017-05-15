2cfa1819652059411830d9427ce69974849ee4b9
David <david.dmr@gmail.com>
2017-05-14 22:54:34 -0300

    Mudança de layout da splash screen

--- app/src/main/java/com/sda/david/fanmovieapp/SplashActivity.java
+++ app/src/main/java/com/sda/david/fanmovieapp/SplashActivity.java
@@ -17,6 +17,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
 
         final PreferencesUtil preferencesUtil = PreferencesUtil.getInstance(this);
 
